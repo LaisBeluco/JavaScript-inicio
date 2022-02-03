235b8efeb69e3ffc4a0abe5e5bf2489f03a0a2b3
Lais B <laisbelucom@gmail.com>
2022-02-02 21:03:36 -0300

    Atualizacao-da-aula

---
+++ aula10/js/index.js
@@ -0,0 +1,23 @@
+let num1 = prompt("Digite um número.");
+num1 = parseFloat(num1);
+
+let num2 = prompt ("Digite outro número.");
+num2 = Number(num2);
+
+const resultado = num1 + num2;
+
+alert('O resultado foi...'+ resultado);
+
+/*
+
+Solução do professor...
+
+let num1 = prompt ('Digite um número.');
+let num2 = prompt ('Digite outro número.')
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
+
+*/
